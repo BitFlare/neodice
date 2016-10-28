e3fd93f8cfdb4990981556ef06cc4a7c08a02f0b
BTCPot <root@flaregg.us>
2016-10-28 16:05:07 +0200

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var config = {
   // - Your app's id on moneypot.com
   app_id: 2365,                             // <----------------------------- EDIT ME!
   // - Displayed in the navbar
-  app_name: 'Flare Network',
+  app_name: 'Rainbow.Dice',
   // - For your faucet to work, you must register your site at Recaptcha
   // - https://www.google.com/recaptcha/intro/index.html
   recaptcha_sitekey: '6LelhQoUAAAAAP5g23Zhg9fdSXyte1HN6kF3QlKH',  // <----- EDIT ME!
